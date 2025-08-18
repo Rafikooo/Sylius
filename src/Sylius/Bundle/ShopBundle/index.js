2da4d149592e5dc0d82a0366c2f3b463e068ec91
Rafikooo <thecrazyrafik@gmail.com>
2025-08-18 10:01:09 +0200

    [Shop][Webpack] Add contenthash-based filenames for JS and CSS files

--- src/Sylius/Bundle/ShopBundle/index.js
+++ src/Sylius/Bundle/ShopBundle/index.js
@@ -64,6 +64,10 @@ class SyliusShop {
                 options.additionalData = `$rootDir: '${rootDir}';`;
             })
             .enableIntegrityHashes(Encore.isProduction())
+            .configureFilenames({
+                js: 'js/[name].[contenthash:8].js',
+                css: 'css/[name].[contenthash:8].css',
+            })
         ;
     }
 }
