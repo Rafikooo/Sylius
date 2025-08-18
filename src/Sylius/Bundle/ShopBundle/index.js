0f02e03c4413e4fb90cdd67aa1155b6637eb2514
Rafikooo <thecrazyrafik@gmail.com>
2025-08-18 09:59:52 +0200

    [Shop] Enable Subresource Integrity (SRI)

--- src/Sylius/Bundle/ShopBundle/index.js
+++ src/Sylius/Bundle/ShopBundle/index.js
@@ -62,7 +62,9 @@ class SyliusShop {
             .enableSassLoader((options) => {
                 // eslint-disable-next-line no-param-reassign
                 options.additionalData = `$rootDir: '${rootDir}';`;
-            });
+            })
+            .enableIntegrityHashes(Encore.isProduction())
+        ;
     }
 }
 
